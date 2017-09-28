6f9874e7a9d4d2d3c6d8e7659dd640b7df7ad84f
Prashan Welipitiya <prshn.weli@gmail.com>
2017-09-28 11:50:06 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # liri-node-app
+# Welcome to Liri. Used to search tweets, songs, and movies. .
+# run app in node and type in commands after file name
+# For tweets type in 'my-tweets'
+# To search up info on a song type in 'spotify-this-song' and then enter the song title.
+# To search up info on a movie type in 'movie-this' and then enter the movie.
+# To run the command in the text file type in 'do-what-it-says'
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
-// twitter section working
 var keys = require('./keys.js');
 
+//-----------------------------------------------------------------------------------------------
+// twitter section working
 function tweet() {
 
 	var Twitter = require('twitter');
@@ -18,9 +19,8 @@ function tweet() {
 
 };
 
-
-// spotify
-
+//-----------------------------------------------------------------------------------------------
+// spotify is now working
 function spot() {
 	var Spotify = require('node-spotify-api');
 
@@ -48,8 +48,77 @@ function spot() {
 
 };
 
+//-----------------------------------------------------------------------------------------------
+// movie-this working
+function movie() {
+	var request = require("request");
+	var nodeArgs = process.argv;
+	var movieName = "";
+
+	for (var i = 3; i < nodeArgs.length; i++) {
+	  if (i > 3 && i < nodeArgs.length) {
+	    movieName = movieName + "+" + nodeArgs[i];
+	  } else {
+			movieName += nodeArgs[i];
+	  }
+	};
+
+	if(process.argv[3] === undefined){
+		movieName = "Mr. Nobody"
+	}
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+	request(queryUrl, function(error, response, body) {
+
+	  if (!error && response.statusCode === 200) {
+	    console.log("Title:", JSON.parse(body).Title);
+	    console.log("Release Year:", JSON.parse(body).Year);
+	    console.log("IMDB Rating:", JSON.parse(body).imdbRating);
+	    console.log("Rotten Tomatoes Rating:", JSON.parse(body).Ratings[1].Value);
+	    console.log("Country:", JSON.parse(body).Country);
+	    console.log("Language:", JSON.parse(body).Language);
+	    console.log("Plot:", JSON.parse(body).Plot);
+	    console.log("Actors:", JSON.parse(body).Actors)
+	  }
+	});
+}
+
+//-----------------------------------------------------------------------------------------------
+//Last thing
+function readingTxt (){
+  var fs = require ("fs");
+
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error){
+      //return console.log(error);
+    }
+
+    var dataArr = data.split(",");
+
+    if (dataArr[0] === 'my-tweets'){
+    	tweet();
+    }
+
+    if (dataArr[0] === 'spotify-this-song'){
+    	process.argv[3] = dataArr[1];
+    	spot();
+    }
+
+    if (dataArr[0] === 'movie-this'){
+    	process.argv[3] = dataArr[1];
+    	movie();
+    }
+  }) 
+}; 
+//-----------------------------------------------------------------------------------------------
+// statements to run program in node
+
 if (process.argv[2] === undefined){
-	console.log("Welcome to Liri. For tweets type in 'my-tweets', to spotify a song type in 'spotify-this-song' and then enter the song title.")
+	console.log("Welcome to Liri. For tweets type in 'my-tweets'");
+	console.log("To search up info on a song type in 'spotify-this-song' and then enter the song title.");
+	console.log("To search up info on a movie type in 'movie-this' and then enter the movie.");
+	console.log("To run the command in the text file type in 'do-what-it-says'");
 }
 
 if (process.argv[2] === 'my-tweets'){
@@ -58,4 +127,12 @@ if (process.argv[2] === 'my-tweets'){
 
 if (process.argv[2] === 'spotify-this-song'){
 	spot();
+};
+
+if (process.argv[2] === 'movie-this'){
+	movie();
+};
+
+if (process.argv[2] === 'do-what-it-says'){
+	readingTxt();
 };
